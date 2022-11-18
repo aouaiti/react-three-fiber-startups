7d4b5a1deec3f11519af944240059f4d113b7229
aouaiti <mr.aouaiti.ahmed@gmail.com>
2022-11-18 16:06:03 +0100

    setting galaxy params in particles scene

--- stories/wrapped/Particules.stories.jsx
+++ stories/wrapped/Particules.stories.jsx
@@ -24,54 +24,60 @@ export default {
 };
 
 const urls = {
-  model1: "/particules/1.png",
-  model2: "/particules/2.png",
-  model3: "/particules/3.png",
-  model4: "/particules/4.png",
-  model5: "/particules/5.png",
-  model6: "/particules/6.png",
-  model7: "/particules/7.png",
-  model8: "/particules/8.png",
-  model9: "/particules/9.png",
-  model10: "/particules/10.png",
-  model11: "/particules/11.png",
-  model12: "/particules/12.png",
-  model13: "/particules/13.png",
+  star1: "/particules/8.png",
+  star2: "/particules/5.png",
+  star3: "/particules/11.png",
+  star4: "/particules/9.png",
+  star5: "/particules/4.png",
+  lense: "/particules/1.png",
+  ring1: "/particules/2.png",
+  ring2: "/particules/7.png",
+  smoke: "/particules/3.png",
+  arc: "/particules/6.png",
+  heart: "/particules/10.png",
+  line1: "/particules/12.png",
+  line2: "/particules/13.png",
 };
 
 const geometries = {
-  geometry1: "fibonacci sphere",
-  geometry2: "stairs",
-  geometry3: "energy",
+  fibonacciSphere: "fibonacci sphere",
+  stairs: "stairs",
+  energy: "energy",
+  galaxy: "galaxy",
 };
 
 const Fragments = () => {
-  const [{ count, url, size, colorize, geometry }, set] = useControls(
-    "particles",
-    () => ({
-      props: folder({
-        count: {
-          value: 10000,
-          min: 100,
-          max: 10000,
-          step: 100,
-        },
-        url: {
-          options: urls,
-        },
-        geometry: {
-          options: geometries,
-        },
-        size: {
-          value: 0.2,
-          min: 0.1,
-          max: 1,
-          step: 0.01,
-        },
-        colorize: true,
-      }),
-    })
-  );
+  const { count, url, size, geometry, branches } = useControls("particles", {
+    props: folder({
+      count: {
+        value: 10000,
+        min: 100,
+        max: 10000,
+        step: 100,
+      },
+      url: {
+        options: urls,
+      },
+      geometry: {
+        options: geometries,
+      },
+      size: {
+        value: 0.2,
+        min: 0.1,
+        max: 1,
+        step: 0.01,
+      },
+    }),
+    galaxy: folder({
+      branches: {
+        value: 3,
+        min: 2,
+        max: 10,
+        step: 1,
+        render: (get) => get("particles.props.geometry") === "galaxy",
+      },
+    }),
+  });
   //   const count = 5000;
   const points = useRef();
   const positions = new Float32Array(count * 3);
@@ -116,7 +122,22 @@ const Fragments = () => {
         colors[i + 2] = Math.random();
       }
     }
-  }, [geometry, count, size, url, colorize]);
+    if (geometry === "galaxy") {
+      for (let i = 0; i < count; i++) {
+        // const branches = 4;
+        const i3 = i * 3;
+        const rdm = ((i % branches) / branches) * Math.PI * 2;
+        const radius = Math.random() * 10;
+        if (i < 20) console.log(rdm);
+        positions[i3] = Math.cos(rdm) * radius;
+        positions[i3 + 1] = 0;
+        positions[i3 + 2] = Math.sin(rdm) * radius;
+        colors[i] = Math.random();
+        colors[i + 1] = Math.random();
+        colors[i + 2] = Math.random();
+      }
+    }
+  }, [geometry, count, size, url, branches]);
   const textures = useTexture({
     map: url,
     alphaMap: url,
@@ -149,7 +170,7 @@ const Fragments = () => {
         sizeAttenuation={true}
         transparent
         {...textures}
-        vertexColors={colorize}
+        vertexColors
         depthWrite={false}
         blending={AdditiveBlending}
       />
